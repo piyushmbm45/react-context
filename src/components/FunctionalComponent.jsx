4c4d6358d7f8f16ae3db4d9fd7c2736048eec5e1
Piyush Jain <piyushmbm45@gmail.com>
2022-03-20 09:02:02 +0530

    using context in functional component

--- src/components/FunctionalComponent.jsx
+++ src/components/FunctionalComponent.jsx
@@ -1,6 +1,17 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../Main';
 
 export const FunctionalComponent = () => {
-    
-  return <div>Functional Component</div>;
+  const theme = useContext(ThemeContext);
+  console.log(theme);
+  function themeStyle(dark) {
+    return {
+      backgroundColor: !dark ? 'red' : 'yellowgreen',
+      color: !dark ? 'white' : 'black',
+      margin: '40px',
+      padding: '50px',
+      borderRadius: !dark ? '10px' : '20px',
+    };
+  }
+  return <div style={themeStyle(theme)}>Functional Component</div>;
 };
